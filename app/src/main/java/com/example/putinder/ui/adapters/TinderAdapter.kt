2f634ecc66c4604a990857cd62d3ef851e6b698f
Bibovsky <48915487+Bibovsky@users.noreply.github.com>
2019-11-30 13:46:07 +0300

    SwipeStack

---
+++ app/src/main/java/com/example/putinder/ui/adapters/TinderAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.putinder.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import com.example.putinder.R
+
+class TinderAdapter(private val mData: ArrayList<String>) :
+    BaseAdapter() {
+    override fun getCount(): Int {
+        return mData.size
+    }
+
+    override fun getItem(position: Int): String {
+        return mData[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(
+        position: Int,
+        convertView: View,
+        parent: ViewGroup
+    ): View {
+        /*var convertView: View? = convertView
+        if (convertView == null) {
+            convertView = LayoutInflater.from(parent.context).inflate(R.layout.tinder_card, parent, false)
+        }
+        val textViewCard =
+            convertView?.findViewById<View>(R.id.textViewCard) as TextView
+        textViewCard.text = mData[position]
+        return convertView*/
+        val view = LayoutInflater.from(parent!!.context).inflate(R.layout.tinder_card,parent,false)
+        return view
+    }
+
+}
