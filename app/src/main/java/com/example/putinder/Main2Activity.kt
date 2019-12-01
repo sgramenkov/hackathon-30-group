651c2b34c9e9a2832b04f6e9dc8f89680d10a91a
Bibovsky <48915487+Bibovsky@users.noreply.github.com>
2019-12-01 16:25:49 +0300

    first start fixed

--- app/src/main/java/com/example/putinder/Main2Activity.kt
+++ app/src/main/java/com/example/putinder/Main2Activity.kt
@@ -25,6 +25,75 @@ class Main2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        /*if (ContextCompat.checkSelfPermission(
+                applicationContext,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            mLocationPermissionGranted = true
+            Log.e("PERMISSION", mLocationPermissionGranted.toString())
+            var locationManager: LocationManager =
+                getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            locationManager.requestLocationUpdates(
+                LocationManager.NETWORK_PROVIDER,
+                1000,
+                0f,
+                object : android.location.LocationListener {
+                    override fun onLocationChanged(location: Location?) {
+
+                        myLat = location!!.latitude
+                        myLon = location!!.longitude
+                        if (myLat!=null && myLon!=null && flag!=true){
+                            descpref = getSharedPreferences("DESCRIPTION",
+                                Context.MODE_PRIVATE
+                            )
+
+                            descprefEditor = descpref.edit()
+                            descprefEditor.putFloat("myLat", myLat!!.toFloat())
+                            descprefEditor.putFloat("myLon",myLon!!.toFloat())
+                            descprefEditor.apply()
+
+
+                            var intent = Intent(applicationContext,MainActivity::class.java)
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                            startActivity(intent)
+                            finish()
+
+                        }
+                        Log.e("COORDS", "$myLat $myLon")
+
+
+
+                        flag=true
+
+
+                    }
+
+                    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+                    }
+
+                    override fun onProviderEnabled(provider: String?) {
+                    }
+
+                    override fun onProviderDisabled(provider: String?) {
+                    }
+
+                })
+
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 1
+            )
+        }*/
+
+
+
+    }
+    override fun onResume() {
+        super.onResume()
         if (ContextCompat.checkSelfPermission(
                 applicationContext,
                 android.Manifest.permission.ACCESS_FINE_LOCATION
@@ -88,7 +157,6 @@ class Main2Activity : AppCompatActivity() {
                 arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 1
             )
         }
-
 
 
     }
--- app/src/main/java/com/example/putinder/MainActivity.kt
+++ app/src/main/java/com/example/putinder/MainActivity.kt
@@ -1,15 +1,31 @@
 package com.example.putinder
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var descpref: SharedPreferences
+    lateinit var descprefEditor: SharedPreferences.Editor
+    var myLat: Double? = null
+    var myLon: Double? = null
+    var mLocationPermissionGranted: Boolean = false
+    private var locationManager: LocationManager? = null
+    var flag:Boolean=false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +47,68 @@ class MainActivity : AppCompatActivity() {
             )
         )
         navView.setupWithNavController(navController)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (ContextCompat.checkSelfPermission(
+                applicationContext,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            mLocationPermissionGranted = true
+            Log.e("PERMISSION", mLocationPermissionGranted.toString())
+            var locationManager: LocationManager =
+                getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            locationManager.requestLocationUpdates(
+                LocationManager.NETWORK_PROVIDER,
+                1000,
+                0f,
+                object : android.location.LocationListener {
+                    override fun onLocationChanged(location: Location?) {
+
+                        myLat = location!!.latitude
+                        myLon = location!!.longitude
+                        if (myLat!=null && myLon!=null){
+                            descpref = getSharedPreferences("DESCRIPTION",
+                                Context.MODE_PRIVATE
+                            )
+
+                            descprefEditor = descpref.edit()
+                            descprefEditor.putFloat("myLat", myLat!!.toFloat())
+                            descprefEditor.putFloat("myLon",myLon!!.toFloat())
+                            descprefEditor.apply()
+
+                        }
+                        Log.e("coordsResume", "$myLat $myLon")
+
+
+
+                        flag=true
+
+
+                    }
+
+                    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+                    }
+
+                    override fun onProviderEnabled(provider: String?) {
+                    }
+
+                    override fun onProviderDisabled(provider: String?) {
+                    }
+
+                })
+
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 1
+            )
+        }
+
 
     }
 }
